371254d197ea0a55981943cf2435550a1b596f09
Netson Cavina <netsoncavina@gmail.com>
2024-08-15 17:12:03 -0300

    initial socket setup

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const {Server} = require('socket.io');
 
 app.use(cors());
-
 const server = http.createServer(app);
-server.listen(3001);
 
+const io = new Server(server, {
+  cors: {
+    // origin: 'http://localhost:3000',
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3001);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
